a4db68ab03bc0ac4caec9625423f968e4c9c5ab6
Laurent Dislaire <siladire@free.fr>
2025-03-16 17:11:40 +0100

    Linter corrections

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
-def add(a,b): 
-    return a+b  
+def add(a, b):
+    return a+b
+
 
 def multiply(x, y):
     return x * y
 
+
 class MyClass:
     def __init__(self):
-        self.value = 0 
+        self.value = 0
--- test_main.py
+++ test_main.py
@@ -4,7 +4,8 @@ from main import add, multiply
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    
+
+
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-1, 1) == -1
