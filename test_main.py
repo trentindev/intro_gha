3ab23deeab2b183aafbffd9481effcce807175bd
Laurent Dislaire <siladire@free.fr>
2025-03-16 16:47:50 +0100

    Test multiply

--- test_main.py
+++ test_main.py
@@ -1,8 +1,11 @@
-from main import add
+from main import add, multiply
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     
-    
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, -1) == 0
